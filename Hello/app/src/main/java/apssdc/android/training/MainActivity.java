9962f58bf798454b25c25411635cb9238e30a38b
sivaprasad <siva.bommini@gmail.com>
2020-06-25 15:53:02 +0530

    hello app added

--- Hello/app/src/main/java/apssdc/android/training/MainActivity.java
+++ Hello/app/src/main/java/apssdc/android/training/MainActivity.java
@@ -3,12 +3,34 @@ package apssdc.android.training;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button count;
+TextView tv;
+int c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count=findViewById(R.id.count_button);
+        tv=findViewById(R.id.text_view);
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c++;
+                tv.setText(""+c);
+                Log.i("msg",""+tv);
+            }
+        });
     }
-}   
+
+
+    public void toast(View view) {
+        Toast.makeText(this,"Value is:  "+c,Toast.LENGTH_SHORT).show();
+    }
+}
