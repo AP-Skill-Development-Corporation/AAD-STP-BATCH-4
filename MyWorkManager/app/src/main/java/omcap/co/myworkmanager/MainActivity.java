cbda8c2444391a3e0300740c9d3f367cf5333134
sivaprasad <siva.bommini@gmail.com>
2020-07-10 16:11:36 +0530

    workmanager and sharedpreferance

---
+++ MyWorkManager/app/src/main/java/omcap/co/myworkmanager/MainActivity.java
@@ -0,0 +1,39 @@
+package omcap.co.myworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.work);
+        text=findViewById(R.id.feed_back);
+        final OneTimeWorkRequest request=new OneTimeWorkRequest.Builder(Mywork.class).build();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkManager.getInstance().enqueue(request);
+            }
+        });
+        WorkManager.getInstance().getWorkInfoByIdLiveData(request.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                String status=workInfo.getState().name();
+                text.append(status+"\n");
+            }
+        });
+
+    }
+}
